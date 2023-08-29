863bed3199ef8360ba4565f0456288ddee3601a9
@duyle <prochief006@gmail.com>
2023-08-29 10:56:48 +0700

    Add project files.

---
+++ Test_IFM/Program.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * This program is developed by DL for demo purpose
+ * Email: prochief006@gmail.com
+ * Date: fisrt demo release 08-2023
+ * *
+ * Description: Demo Connect IFM Vision O2I5xx with C# using external trigger (optical sensor)
+ */
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Test_IFM
+{
+    class Program
+    {
+        private static TcpClient client;
+        private static NetworkStream clientStream;
+        private static readonly byte[] data = new byte[1024];       
+        public static event Action<string> DataReceived;
+        private const string EmptyString = "star;0;;;stop"; // my sensor return this string when no code is detected
+        private const string IPAdress = "192.168.64.2"; // change IP address if needed
+        private const int PCIC_Port = 50010; // default port of IFM PCIC
+        static void Main(string[] args)
+        {
+            try
+            {
+                client = new TcpClient(IPAdress, PCIC_Port);
+                clientStream = client.GetStream();
+
+                // Khởi động luồng lắng nghe dữ liệu
+                Thread listenThread = new Thread(ListenForData);
+                listenThread.Start();
+
+                // Chờ sự kiện dữ liệu nhận được và xử lý
+                DataReceived += HandleReceivedData;
+
+                // Vòng lặp chính hoặc bất kỳ xử lý khác 
+                while (true)
+                {
+                    // Thực hiện các thao tác khác ở đây
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while listening for data: " + ex.Message);
+                // process lost connection here
+            }
+
+        }
+       
+        private static void ListenForData()
+        {
+            try 
+            {
+                while (true)
+                {
+                    
+                    int bytesRead;
+                    // Read the first batch of the TcpServer response bytes. 
+                    bytesRead = clientStream.Read(data, 0, data.Length);
+                    string receivedData = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                    // Read the second batch of the TcpServer response bytes.
+                    bytesRead = clientStream.Read(data, 0, data.Length);
+                    receivedData += Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                    // response data sampel: 2 codes with content "ABC-abc-1234"
+                    //1234L000000043{0D}{0A}1234star;1;ABC-abc-1234;ABC-abc-1234;stop
+                    //ticket + length + /r/n + ticket + data + /r/n
+                    //extract data from receive string
+                    //extract length information
+                    int lengthData = int.Parse(receivedData.Substring(5, 9));
+                    string stringData = receivedData.Substring(20, (lengthData - 6));
+
+                    // Raise the DataReceived event
+                    OnDataReceived(stringData);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while listening for data: " + ex.Message);
+                // process lost connection here
+            }
+            finally
+            {
+                client.Close();
+                clientStream.Close();
+                Console.WriteLine("Connection closed.");
+            }
+
+        }
+
+        private static void OnDataReceived(string receivedData)
+        {
+            // Khi dữ liệu nhận được, gọi sự kiện DataReceived
+            DataReceived?.Invoke(receivedData);
+        }
+        private static void HandleReceivedData(string receivedData)
+        {
+            try
+            {
+               
+                if (receivedData.StartsWith("star") && receivedData.EndsWith("stop"))
+                {
+                    // data is empty
+                    if (receivedData == EmptyString)
+                    {
+                        // cant find the code
+                        Console.WriteLine("Code is Empty!");
+                    }
+                    else
+                    {
+                        string[] parts = receivedData.Split(';');
+                        string qrCode = parts[2];
+                        string content = parts[3];  
+                        if(parts.Length >= 4)
+                        {                            
+                            Console.WriteLine("Detected {0} with content:{1} " , qrCode, content);
+                        }
+                        else 
+                        {                           
+                            Console.WriteLine("Data is invalid, use IFM vision assistant config sensor and try again!");                                
+                        }
+                    }                   
+                }
+                else 
+                { 
+                    Console.WriteLine("Data is invalid, use IFM vision assistant confid sensor and try again !");
+                }               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+      
+    }
+}
---
+++ Test_IFM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test_IFM")]
+[assembly: AssemblyDescription("This is demo code for IFM 02I5xx")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DL")]
+[assembly: AssemblyProduct("Test_IFM")]
+[assembly: AssemblyCopyright("Copyright © DuyLe 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8f174d8-c5c3-4425-8f49-7678e1eb12f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
